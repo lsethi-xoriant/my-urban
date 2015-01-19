5a9647867d6a023bffb3af52c5e45f142fd0b29c
mykolachernyshov <mykolaror@gmail.com>
2015-01-19 12:08:39 +0200

    destroy all

--- db/migrate/20150118162809_add_fields_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddFieldsToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :first_name, :string
-    add_column :users, :last_name, :string
-    add_column :users, :about_user, :string
-  end
-end
