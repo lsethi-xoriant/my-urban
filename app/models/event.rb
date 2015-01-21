b040b6fa81a6084f12e5fd3aa86457e791f86fa6
mykolachernyshov <mykolaror@gmail.com>
2015-01-21 11:15:28 +0200

    added cancancan gem for giving users permision

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ActiveRecord::Base
 	validates_presence_of :name, :adress, :description, :data, :timeStart, :endTime
-	validates :data , allow_blank: true, format: { with: /\d{2}.\d{2}.\d{4}/,
+	validates :data , allow_blank: true, format: { with: /\d{2}\.\d{2}\.\d{4}/,
 		message: "only allows data dd.mm.yyyy" }
 	validates :timeStart, :endTime, allow_blank: true, format: { with: /\d{2}:\d{2}/,
 	  message: "only allows time format hh:mm" }
