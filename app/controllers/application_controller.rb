ea1e52cfd52cf88f7993e8e8666350372277dc71
taras2358 <taras2358@gmail.com>
2015-04-27 18:50:38 +0300

    added tasks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  #TODO make tasks for avatar destroy(wich don't have event)
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
