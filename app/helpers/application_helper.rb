c218fa3311e05f6f2981512105663378072e65ed
taras2358 <taras2358@gmail.com>
2015-04-29 19:17:10 +0300

    added modal window for pictures and fix avatars

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,6 @@
 module ApplicationHelper
   #TODO make avatar uploader similar to image uploader for event
   #TODO make slide show for picture collection
+  #a href="#{event.avatar.avatar_url(:large_image)}" data-lightbox="image-1" data-title="My caption"
+  #TODO make acsept invite to event
 end
