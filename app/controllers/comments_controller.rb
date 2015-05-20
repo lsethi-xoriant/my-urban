c53b3e321313cc93b811844b4e31ef05055da7c8
taras2358 <taras2358@gmail.com>
2015-05-20 18:54:03 +0300

    added actionMailer

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,8 @@ class CommentsController < ApplicationController
   def create
     @event = Event.find(params[:event_id])
     @comment = @event.comments.create(comment_params)
+    name = current_user.first_name + " " + current_user.last_name
+    CommentsMailer.comment_to_event(name, @event.user.email, @comment.body, current_user.email) if @comment.valid? 
     redirect_to event_path(@event)
   end
  
