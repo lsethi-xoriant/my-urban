9259f98ec9e33332952926ab1687009650ad7562
taras2358 <taras2358@gmail.com>
2015-05-27 20:43:06 +0300

    todo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,4 @@
 module ApplicationHelper
-  #TODO make avatar uploader similar to image uploader for event
-  #TODO make slide show for picture collection
-  #a href="#{event.avatar.avatar_url(:large_image)}" data-lightbox="image-1" data-title="My caption"
-  #TODO make acsept invite to event
+  #TODO user verify before votozvit upload
+  #TODO image preload for image of event
 end
