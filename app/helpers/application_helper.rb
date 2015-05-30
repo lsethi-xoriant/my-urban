7572fa497f95f937b9f6ce35ea892ff97f52a503
taras2358 <taras2358@gmail.com>
2015-05-30 17:26:56 +0300

    added features

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,6 @@
 module ApplicationHelper
   #TODO fix google autocomplete 
   #TODO change count of small group
+  #TODO on event show now are showing invites and comes users
+  #TODO places incude invite and come people
 end
